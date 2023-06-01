a07b2f6714e4080cf46e72a693b8ae3fb78f50ab
Ткачук Алексей <box03-853@yandex.ru>
2023-06-01 09:45:02 +0400

    Initial commit

---
+++ src/If.java
@@ -0,0 +1,12 @@
+public class If {
+    public static void main(String [] args) {
+        int myInt = 21;
+        if(myInt < 10) {
+            System.out.println("дф, верно");
+        } else if(myInt > 20) {
+            System.out.println("нет, не верно");
+        } else {
+            System.out.println("ни один из приведенных примеров");
+        }
+    }
+}
